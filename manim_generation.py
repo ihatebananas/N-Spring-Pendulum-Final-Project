7d994f9d037e10b27961939666b4e582d74b7d87
ihatebananas <shravantimmy@gmail.com>
2025-04-12 20:34:31 -0400

    finished generalizing manim file as far as adding masses and springs. Should consider generalizing parameters like spring width.

--- manim_generation.py
+++ manim_generation.py
@@ -5,7 +5,7 @@ from NSP import NSP
 class SpringPendulum(Scene):
     
     def construct(self):
-        def update_spring(Point1, Point2, zags):
+        def update_spring(Point1, Point2, zags, width):
             center_1 = Point1.get_center()
             center_2 = Point2.get_center()
 
@@ -14,7 +14,7 @@ class SpringPendulum(Scene):
             normal_to_vec = np.zeros_like(vec)
             normal_to_vec[0] = vec[1]
             normal_to_vec[1] = -vec[0]
-            normal_to_vec = 0.3*normal_to_vec / np.linalg.norm(normal_to_vec)
+            normal_to_vec = width*normal_to_vec / np.linalg.norm(normal_to_vec)
 
             corners = np.zeros((3*zags - 1, 3))
             corners[0] = center_1
@@ -36,50 +36,46 @@ class SpringPendulum(Scene):
 
             return spring
 
-        M_vec = [10, 10, 10]
-        K_vec = [500, 500, 500]
-        L_vec = [2, 2, 2]
+        N = 3
+        M_vec = np.zeros(N) + 10
+        K_vec = np.zeros(N) + 1000
+        L_vec = np.zeros(N) + 1
         spring_pendulum = NSP(M_vec, K_vec, L_vec)
 
-        x_0 = [1, 2, 3]
-        y_0=[1, 2, 3]
-        x_dot_0 = [0, 0, 0]
-        y_dot_0 = [0, 0, 0]
-        t_pts = np.arange(0, 10, 0.02)
+        x_0 = [0 for i in range(1, N+1)]
+        y_0=[-i for i in range(1, N+1)] 
+        x_dot_0 = np.zeros(N) + 2
+        y_dot_0 = np.zeros(N)
+        t_pts = np.arange(0, 5, 0.02)
 
         solution = spring_pendulum.solve_ode(x_0, y_0, x_dot_0, y_dot_0, t_pts)
 
-        x1 = solution[0, :]/2.25
-        x2 = solution[1, :]/2.25
-        x3 = solution[2, :]/2.25
-        y1 = solution[3, :]/2.25
-        y2 = solution[4, :]/2.25
-        y3 = solution[5, :]/2.25
+        pivot = Dot().move_to(2*UP)
+        Mass_Vector = [Dot() for i in range(N)]
+        Spring_Vector = [VMobject() for i in range(N)]
+        spr_width = 0.3
+        zags = 10
 
-        pivot = Dot().move_to(1*UP)
-        Mass_1= Dot(radius=0.03*M_vec[0]).move_to(x1[0]*RIGHT+(y1[0]+1)*UP).set_color(BLUE)
-        Mass_2= Dot(radius=0.03*M_vec[1]).move_to(x2[0]*RIGHT+(y2[0]+1)*UP).set_color(BLUE)
-        Mass_3= Dot(radius=0.03*M_vec[2]).move_to(x3[0]*RIGHT+(y3[0]+1)*UP).set_color(BLUE)
+        x1 = solution[0, :]
+        y1 = solution[N, :]
+        Mass_Vector[0] = Dot(radius=0.02*M_vec[0]).move_to(x1[0]*RIGHT+(y1[0]+2)*UP).set_color(BLUE)
+        Spring_Vector[0] = update_spring(pivot, Mass_Vector[0], zags, spr_width)        
+        Spring_Vector[0].add_updater(lambda x: x.become(update_spring(pivot, Mass_Vector[0], zags, spr_width)))
 
-        Spring_1 = update_spring(pivot, Mass_1, 8)        
-        Spring_1.add_updater(lambda x: x.become(update_spring(pivot, Mass_1, 8)))
-        Spring_2 = update_spring(Mass_1, Mass_2, 8)        
-        Spring_2.add_updater(lambda x: x.become(update_spring(Mass_1, Mass_2, 8)))
-        Spring_3 = update_spring(Mass_2, Mass_3, 8)        
-        Spring_3.add_updater(lambda x: x.become(update_spring(Mass_2, Mass_3, 8)))
+        for k in range(1,N):
+            xk = solution[k, :]
+            yk = solution[k+N, :]
+            Mass_Vector[k] = Dot(radius=0.02*M_vec[k]).move_to(xk[0]*RIGHT+(yk[0]+2)*UP).set_color(BLUE)
+            Spring_Vector[k] = update_spring(Mass_Vector[k-1], Mass_Vector[k], zags, spr_width)
+            Spring_Vector[k].add_updater(lambda x, k=k: x.become(update_spring(Mass_Vector[k-1], Mass_Vector[k], zags, spr_width)))
 
-        self.add(pivot, Mass_1, Spring_1, Mass_2, Spring_2, Mass_3, Spring_3)
+        self.add(*Mass_Vector)
+        self.add(*Spring_Vector)
+        self.add(pivot)
 
         for i in range(len(x1) - 1):
-            self.remove(Mass_1)
-            self.remove(Mass_2)
-            self.remove(Mass_3)
-            self.add(Mass_1)
-            self.add(Mass_2)
-            self.add(Mass_3)
-            
-            self.play(
-                    Mass_1.animate.move_to(x1[i+1]*RIGHT + (y1[i+1]+1)*UP),
-                    Mass_2.animate.move_to(x2[i+1]*RIGHT + (y2[i+1]+1)*UP),
-                    Mass_3.animate.move_to(x3[i+1]*RIGHT + (y3[i+1]+1)*UP),
-                    run_time=1/50,rate_func=linear)
+            self.remove(*Mass_Vector)
+            self.add(*Mass_Vector)
+            animations = [ApplyMethod(Mass_Vector[m].move_to, solution[m, :][i+1]*RIGHT + (solution[m+N, :][i+1]+2)*UP) for m in range(N)]
+
+            self.play(*animations, run_time=1/50, rate_func=linear)
