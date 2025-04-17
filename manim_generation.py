ce5b2a9b648fa9b51f3223e0b502f00dd4d794e7
ihatebananas <shravantimmy@gmail.com>
2025-04-16 23:03:57 -0400

    added plots and manimations

--- manim_generation.py
+++ manim_generation.py
@@ -36,36 +36,37 @@ class SpringPendulum(Scene):
 
             return spring
 
-        N = 3
-        M_vec = np.zeros(N) + 10
-        K_vec = np.zeros(N) + 1000
-        L_vec = np.zeros(N) + 1
+        N = 5
+        zags = 8 + int(np.sqrt(N))
+        spr_width = 1/N
+
+        M_vec = np.zeros(N) + 25/N
+        K_vec = np.zeros(N) + 250*N
+        L_vec = np.zeros(N) + spr_width*5
         spring_pendulum = NSP(M_vec, K_vec, L_vec)
 
         x_0 = [0 for i in range(1, N+1)]
-        y_0=[-i for i in range(1, N+1)] 
-        x_dot_0 = np.zeros(N) + 2
+        y_0=[-i*(spr_width*5) for i in range(1, N+1)] 
+        x_dot_0 = np.zeros(N) + 1 + 10/N
         y_dot_0 = np.zeros(N)
-        t_pts = np.arange(0, 5, 0.02)
+        t_pts = np.arange(0, 20, 0.02)
 
         solution = spring_pendulum.solve_ode(x_0, y_0, x_dot_0, y_dot_0, t_pts)
 
-        pivot = Dot().move_to(2*UP)
+        pivot = Dot().move_to(3*UP)
         Mass_Vector = [Dot() for i in range(N)]
         Spring_Vector = [VMobject() for i in range(N)]
-        spr_width = 0.3
-        zags = 10
 
         x1 = solution[0, :]
         y1 = solution[N, :]
-        Mass_Vector[0] = Dot(radius=0.02*M_vec[0]).move_to(x1[0]*RIGHT+(y1[0]+2)*UP).set_color(BLUE)
+        Mass_Vector[0] = Dot(radius=0.02*M_vec[0]).move_to(x1[0]*RIGHT+(y1[0]+3)*UP).set_color(BLUE)
         Spring_Vector[0] = update_spring(pivot, Mass_Vector[0], zags, spr_width)        
         Spring_Vector[0].add_updater(lambda x: x.become(update_spring(pivot, Mass_Vector[0], zags, spr_width)))
 
         for k in range(1,N):
             xk = solution[k, :]
             yk = solution[k+N, :]
-            Mass_Vector[k] = Dot(radius=0.02*M_vec[k]).move_to(xk[0]*RIGHT+(yk[0]+2)*UP).set_color(BLUE)
+            Mass_Vector[k] = Dot(radius=0.02*M_vec[k]).move_to(xk[0]*RIGHT+(yk[0]+3)*UP).set_color(BLUE)
             Spring_Vector[k] = update_spring(Mass_Vector[k-1], Mass_Vector[k], zags, spr_width)
             Spring_Vector[k].add_updater(lambda x, k=k: x.become(update_spring(Mass_Vector[k-1], Mass_Vector[k], zags, spr_width)))
 
@@ -76,6 +77,6 @@ class SpringPendulum(Scene):
         for i in range(len(x1) - 1):
             self.remove(*Mass_Vector)
             self.add(*Mass_Vector)
-            animations = [ApplyMethod(Mass_Vector[m].move_to, solution[m, :][i+1]*RIGHT + (solution[m+N, :][i+1]+2)*UP) for m in range(N)]
+            animations = [ApplyMethod(Mass_Vector[m].move_to, solution[m, :][i+1]*RIGHT + (solution[m+N, :][i+1]+3)*UP) for m in range(N)]
 
             self.play(*animations, run_time=1/50, rate_func=linear)
